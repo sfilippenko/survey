df7b2d73d7203d898c879b98e26db63285aaf4a7
sfilippenko <fil_mf@mail.ru>
2021-07-15 18:05:45 +0300

    Убрал ненужный css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import './App.css';
+import { FC } from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from './styles/theme';
 import Layout from './components/Layout';
 import Root from './components/Root';
 
-function App() {
+const App: FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
@@ -15,6 +14,6 @@ function App() {
       </Layout>
     </ThemeProvider>
   );
-}
+};
 
 export default App;
